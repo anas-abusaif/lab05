0a609d4e962bb188073aafad53996a90b74292f1
AnasAbusief <>
2021-05-30 18:28:03 +0000

    added lab05

---
+++ script.js
@@ -0,0 +1,5 @@
+var userAge = prompt('please enter your age!')
+while (userAge<20 && userAge>40){
+  alert('only people between the ages 20 to 40 are allowed')
+}
+var rating = prompt('from 1 to 10 how much would you rate my website?')
