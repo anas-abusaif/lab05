28ec58c8a166405e8e5ffb404124739e4fe138ec
AnasAbusief <>
2021-05-30 19:19:59 +0000

    update

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var userAge = prompt('please enter your age!')
-while (userAge<20 && userAge>40){
-  alert('only people between the ages 20 to 40 are allowed')
+while (userAge != 20){
+  alert('only people between the ages 20 are allowed')
 }
 var rating = prompt('from 1 to 10 how much would you rate my website?')
+for(i=0; i<rating; i++){
+  document.write('<img src= "https://www.crownconnect.com/assets/ThankYou.jpg"/>')
+}
+ 
